2362daa12ad261addabc33929efe5fb33c6a3547
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2024-11-20 21:43:00 -0500

    Update ShycatApplicationTests

--- backend/src/test/java/fyi/shycat/site/ShycatApplicationTests.java
+++ backend/src/test/java/fyi/shycat/site/ShycatApplicationTests.java
@@ -1,13 +1,23 @@
 package fyi.shycat.site;
 
+import fyi.shycat.site.rest.EventController;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest
 class ShycatApplicationTests {
 
+    @Inject
+    private EventController eventController;
+
     @Test
     void contextLoads() {
+        assertThat(eventController, is(notNullValue()));
     }
 
 }
