dc1e6b038e0c2a8ef6e023bd5d47d4d3d24bac39
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2024-11-20 21:22:46 -0500

    Add base Spring Boot application

---
+++ backend/src/main/java/fyi/shycat/site/ShycatApplication.java
@@ -0,0 +1,13 @@
+package fyi.shycat.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShycatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShycatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fyi/shycat/site/ShycatApplicationTests.java
@@ -0,0 +1,13 @@
+package fyi.shycat.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShycatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
