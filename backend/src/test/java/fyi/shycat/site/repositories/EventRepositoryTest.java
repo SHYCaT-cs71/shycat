0bec9a3b47484784a6d480d6aede16c731fb4350
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2024-11-27 18:06:47 -0500

    Update test input data and repository test for Event changes

--- backend/src/test/java/fyi/shycat/site/repositories/EventRepositoryTest.java
+++ backend/src/test/java/fyi/shycat/site/repositories/EventRepositoryTest.java
@@ -2,18 +2,16 @@ package fyi.shycat.site.repositories;
 
 import fyi.shycat.site.entities.DateTime;
 import fyi.shycat.site.entities.Event;
+import fyi.shycat.site.entities.Location;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.OffsetTime;
-import java.time.ZoneOffset;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.Set;
@@ -24,7 +22,6 @@ import static org.hamcrest.Matchers.*;
 @Sql("/event_input.sql")
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EventRepositoryTest {
 
     @Inject
@@ -37,14 +34,21 @@ public class EventRepositoryTest {
         event.setSummary("An exciting showcase where Harvard entrepreneurs and innovators present their startup ideas.");
         event.setDescription("This annual event brings together the Harvard community to witness and support the entrepreneurial ideas of Harvard's brightest minds. Innovators pitch their projects to potential investors, gaining invaluable feedback and visibility. From tech startups to social enterprises, attendees will see a diverse array of ventures aimed at addressing real-world issues. It’s also a great networking opportunity for anyone interested in entrepreneurship and innovation.\",\n");
         event.setStartDateTime(new DateTime(LocalDate.of(2024, 11, 25),
-                                            OffsetTime.of(LocalTime.of(19, 20, 47), ZoneOffset.ofHours(-5))));
+                                            LocalTime.of(19, 20, 47)));
         event.setEndDateTime(new DateTime(LocalDate.of(2024, 11, 25),
-                                          OffsetTime.of(LocalTime.of(22, 5, 30), ZoneOffset.ofHours(-5))));
+                                          LocalTime.of(22, 5, 30)));
 
         event.setHost("Harvard Innovation Labs");
         event.setOriginalLink("https://example.com/harvard-startup-showcase");
         event.setTags(Set.of("Entrepreneurship", "Startups", "Networking"));
         event.setImageUrl("https://picsum.photos/800/600?random=1003");
+
+        var location = new Location();
+        location.setName("Sanders Theatre, Cambridge, MA");
+        location.setAddress("11 Divinity Avenue, Cambridge");
+        var geo = new Location.GeoLocation(42.377512, -71.114127);
+        location.setGeo(geo);
+        event.setLocation(location);
 
         assertThat(event.getId(), is(nullValue()));
 
@@ -56,10 +60,38 @@ public class EventRepositoryTest {
     }
 
     @Test
-    void testGet() {
+    void testGet_FullLocation_DateTime() {
         var result = eventRepository.findById(1L);
         assertThat(result.isPresent(), is(true));
         checkEvent(result.get());
+    }
+
+    @Test
+    void testGet_LocationNameOnly() {
+        var result = eventRepository.findById(3L);
+        assertThat(result.isPresent(), is(true));
+
+        var event = result.get();
+        assertThat(event.getLocation(), is(notNullValue()));
+        // Just check location, other property load is already checked
+        var location = event.getLocation();
+        assertThat(location.getName(), is("Harvard Hall, Cambridge, MA"));
+        assertThat(location.getAddress(), is(nullValue()));
+        assertThat(location.getGeo(), is(nullValue()));
+    }
+
+    @Test
+    void testGet_NoLocation_DateOnly() {
+        var result = eventRepository.findById(5L);
+        assertThat(result.isPresent(), is(true));
+
+        var event = result.get();
+        // Just check location and start/end times, other property load is already checked
+        assertThat(event.getLocation(), is(nullValue()));
+        assertThat(event.getStartDateTime().getDate(), is(LocalDate.of(2024, 12, 20)));
+        assertThat(event.getStartDateTime().getTime(), is(nullValue()));
+        assertThat(event.getEndDateTime().getDate(), is(LocalDate.of(2024, 12, 22)));
+        assertThat(event.getEndDateTime().getTime(), is(nullValue()));
     }
 
     void checkEvent(Event event) {
@@ -69,12 +101,18 @@ public class EventRepositoryTest {
         assertThat(event.getDescription(), is("This annual event brings together the Harvard community to witness and support the entrepreneurial ideas of Harvard's brightest minds. Innovators pitch their projects to potential investors, gaining invaluable feedback and visibility. From tech startups to social enterprises, attendees will see a diverse array of ventures aimed at addressing real-world issues. It’s also a great networking opportunity for anyone interested in entrepreneurship and innovation."));
         assertThat(event.getStartDateTime().getDate(), is(LocalDate.of(2024, 11, 30)));
         assertThat(event.getStartDateTime().getTime(),
-                   is(OffsetTime.of(LocalTime.of(18, 0, 0), ZoneOffset.ofHours(-5))));
+                   is(LocalTime.of(18, 0, 0)));
         assertThat(event.getEndDateTime().getDate(), is(LocalDate.of(2024, 11, 30)));
         assertThat(event.getEndDateTime().getTime(),
-                   is(OffsetTime.of(LocalTime.of(20, 30, 0), ZoneOffset.ofHours(-5))));
+                   is(LocalTime.of(20, 30, 0)));
         assertThat(event.getHost(), is("Harvard Innovation Labs"));
-//        assertThat(event.getLocation(), is("Harvard Innovation Labs, Allston, MA"));
+        assertThat(event.getLocation(), is(notNullValue()));
+        var location = event.getLocation();
+        assertThat(location.getName(), is("Harvard i-lab, Batten Hall"));
+        assertThat(location.getAddress(), is("125 Western Ave, Allston"));
+        assertThat(location.getGeo(), is(notNullValue()));
+        assertThat(location.getGeo().getLatitude(), is(closeTo(42.36405444183414, 1e-8)));
+        assertThat(location.getGeo().getLongitude(), is(closeTo(-71.12418004499675, 1e-8)));
         assertThat(event.getImageUrl(), is("https://picsum.photos/800/600?random=0.1"));
         assertThat(event.getOriginalLink(), is("https://example.com/harvard-startup-showcase"));
         assertThat(event.getTags(), containsInAnyOrder("Entrepreneurship","Startups","Networking"));
