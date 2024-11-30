064a3d10dbe65f504249f81cf048e76bfc430e1d
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2024-11-30 11:28:13 -0500

    Update serialization test to check output when no host is set

--- backend/src/test/java/fyi/shycat/site/entities/EventJsonMappingTest.java
+++ backend/src/test/java/fyi/shycat/site/entities/EventJsonMappingTest.java
@@ -22,6 +22,7 @@ public class EventJsonMappingTest {
     void testEventSerialization_FullProperties() throws Exception {
         // Test serialization of Event with values for all properties
         var event = createBaseEvent(1L);
+        event.setHost("Event host");
         event.setStartDateTime(new DateTime(LocalDate.of(2024, 11, 27),
                                             LocalTime.of(20, 5, 15)));
         event.setEndDateTime(new DateTime(LocalDate.of(2024, 11, 28),
@@ -36,6 +37,7 @@ public class EventJsonMappingTest {
         checkBaseEvent(json, 1);
         assertThat(json).extractingJsonPathStringValue("$.startDate").isEqualTo("2024-11-27T20:05:15");
         assertThat(json).extractingJsonPathStringValue("$.endDate").isEqualTo("2024-11-28T10:36:44");
+        assertThat(json).extractingJsonPathStringValue("$.host").isEqualTo("Event host");
         assertThat(json).extractingJsonPathStringValue("$.locationName").isEqualTo("Location name");
         assertThat(json).extractingJsonPathStringValue("$.locationAddress").isEqualTo("123 Main St, Cambridge");
         assertThat(json).extractingJsonPathNumberValue("$.locationGeo.latitude").isEqualTo(41.3);
@@ -46,6 +48,7 @@ public class EventJsonMappingTest {
     void testEventSerialization_DateOnly_LocationNameOnly() throws Exception {
         // Test serialization of Event with only dates (no times) and only location name
         var event = createBaseEvent(2L);
+        event.setHost("Event host");
         event.setStartDateTime(new DateTime(LocalDate.of(2024, 11, 27), null));
         event.setEndDateTime(new DateTime(LocalDate.of(2024, 11, 28), null));
         var location = new Location();
@@ -56,21 +59,24 @@ public class EventJsonMappingTest {
         checkBaseEvent(json, 2);
         assertThat(json).extractingJsonPathStringValue("$.startDate").isEqualTo("2024-11-27");
         assertThat(json).extractingJsonPathStringValue("$.endDate").isEqualTo("2024-11-28");
+        assertThat(json).extractingJsonPathStringValue("$.host").isEqualTo("Event host");
         assertThat(json).extractingJsonPathStringValue("$.locationName").isEqualTo("Location name");
         assertThat(json).hasEmptyJsonPathValue("$.locationAddress");
         assertThat(json).hasEmptyJsonPathValue("$.locationGeo");
     }
 
     @Test
-    void testEventSerialization_NoLocation_NoEndDate() throws Exception {
-        // Test serialization of Event with no location or end date
+    void testEventSerialization_NoLocation_NoEndDate_NoHost() throws Exception {
+        // Test serialization of Event with no location, host, or end date
         var event = createBaseEvent(3L);
         event.setStartDateTime(new DateTime(LocalDate.of(2024, 11, 27), null));
 
         var json = jacksonTester.write(event);
+        System.out.println(json);
         checkBaseEvent(json, 3);
         assertThat(json).extractingJsonPathStringValue("$.startDate").isEqualTo("2024-11-27");
         assertThat(json).hasEmptyJsonPathValue("$.endDate");
+        assertThat(json).hasEmptyJsonPathValue("$.host");
         assertThat(json).hasEmptyJsonPathValue("$.locationName");
         assertThat(json).hasEmptyJsonPathValue("$.locationAddress");
         assertThat(json).hasEmptyJsonPathValue("$.locationGeo");
@@ -82,7 +88,6 @@ public class EventJsonMappingTest {
         event.setTitle("Event Title");
         event.setSummary("Event has a slightly longer summary.");
         event.setDescription("Event description should be even longer than the summary.");
-        event.setHost("Event host");
         event.setOriginalLink("https://www.nonsense.com/original_link");
         event.setImageUrl("https://www.nonsense.com/image_url");
         event.setTags(Set.of("Tag A", "Tag B"));
@@ -96,7 +101,6 @@ public class EventJsonMappingTest {
                         .isEqualTo("Event has a slightly longer summary.");
         assertThat(json).extractingJsonPathStringValue("$.description")
                         .isEqualTo("Event description should be even longer than the summary.");
-        assertThat(json).extractingJsonPathStringValue("$.host").isEqualTo("Event host");
         assertThat(json).extractingJsonPathStringValue("$.originalLink").isEqualTo("https://www.nonsense.com/original_link");
         assertThat(json).extractingJsonPathStringValue("$.imageUrl").isEqualTo("https://www.nonsense.com/image_url");
         assertThat(json).extractingJsonPathArrayValue("$.tags").containsExactlyInAnyOrder("Tag A", "Tag B");
