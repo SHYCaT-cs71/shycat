c87880a8f5446a243c3ad2f33175c697b806c526
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2024-12-08 17:53:51 -0500

    S3: Update WebScraper interface to throw IOException

--- backend/src/main/java/fyi/shycat/site/web_scraping/WebScraper.java
+++ backend/src/main/java/fyi/shycat/site/web_scraping/WebScraper.java
@@ -2,10 +2,11 @@ package fyi.shycat.site.web_scraping;
 
 import fyi.shycat.site.entities.Event;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface WebScraper {
 
-    List<Event> getEvents(LocalDate date);
+    List<Event> getEvents(LocalDate date) throws IOException;
 }
