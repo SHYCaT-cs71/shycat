6a16dd415fb00a54d92a0c2abdc85388c211f8df
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2024-12-09 19:45:39 -0500

    S3: Update configuration to use scheduling

--- backend/src/main/java/fyi/shycat/site/ShycatApplication.java
+++ backend/src/main/java/fyi/shycat/site/ShycatApplication.java
@@ -2,8 +2,10 @@ package fyi.shycat.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShycatApplication {
 
 	public static void main(String[] args) {
