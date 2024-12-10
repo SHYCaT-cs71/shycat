2f8daf0f08f76a4c97a4b3ad2fbc1d3066e12fe0
Christina Molodowitch <37000496+cmolodowitch@users.noreply.github.com>
2024-12-09 19:44:41 -0500

    S3: Refactor to use new static helper method

--- backend/src/test/java/fyi/shycat/site/rest/EventControllerUnitTest.java
+++ backend/src/test/java/fyi/shycat/site/rest/EventControllerUnitTest.java
@@ -1,7 +1,5 @@
 package fyi.shycat.site.rest;
 
-import fyi.shycat.site.entities.DateTime;
-import fyi.shycat.site.entities.Event;
 import fyi.shycat.site.repositories.EventRepository;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -12,9 +10,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDate;
 import java.util.Arrays;
 
+import static fyi.shycat.site.repositories.EventRepositoryTest.createEvent;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -41,14 +39,6 @@ class EventControllerUnitTest {
            .andExpect(jsonPath("$.*", hasSize(2)))
            .andExpect(jsonPath("$[0].title", is("Event 1")))
            .andExpect(jsonPath("$[1].title", is("Event 2")));
-    }
-
-    Event createEvent(Long id, String title) {
-        var event = new Event();
-        event.setId(id);
-        event.setTitle(title);
-        event.setStartDateTime(new DateTime(LocalDate.of(2024, 11, 27)));
-        return event;
     }
 }
 
