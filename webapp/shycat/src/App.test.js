a88e2a5c64cbfa5fc3bed7cf0c1e4a0d25f3dbf5
Hamza Hashmi <muh350@g.harvard.edu>
2024-12-11 19:53:28 -0500

    Update Front end to remove mock events

--- webapp/shycat/src/App.test.js
+++ webapp/shycat/src/App.test.js
@@ -7,17 +7,3 @@ test('renders navbar', () => {
   const navbarElement = screen.getAllByRole("navigation");
   expect(navbarElement[0]).toBeInTheDocument();
 });
-
-test('renders EventList with events', () => {
-  render(<App />);
-  const eventCards = screen.getAllByTestId('event-card');
-  expect(eventCards.length).toBe(mockHarvardEvents.length);
-});
-
-test('EventList renders events with correct titles', () => {
-  render(<App />);
-  mockHarvardEvents.forEach((event) => {
-    const eventTitle = screen.getByText(event.title);
-    expect(eventTitle).toBeInTheDocument();
-  });
-});
